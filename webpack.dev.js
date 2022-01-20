0915ed2f0ca74dd16803c24b5a00d1ee3f1d9ca3
ZakharDay <zakhar.day@gmail.com>
2022-01-20 18:24:44 +0300

    VirtualBox setup

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,12 +6,12 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './build',
+    contentBase: './dev_build',
     inline: true,
     host: '10.0.2.15',
     port: 8080
   },
-  watch: true,
+  // watch: true,
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dev_build')
