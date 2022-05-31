4250df9ad0e2c890dcbaf591ebaafc6e1cc8b05f
ZakharDay <zakhar.day@gmail.com>
2022-05-31 18:38:34 +0300

    Trying to send event

--- src/javascript/store/configureStore.prod.js
+++ src/javascript/store/configureStore.prod.js
@@ -1,15 +1,3 @@
-// import { createStore, applyMiddleware } from 'redux'
-// import thunk from 'redux-thunk'
-// // import api from '../middleware/api'
-// import rootReducer from '../reducers'
-//
-// const configureStore = (preloadedState) => {
-//   console.log('We now use prod store')
-//   createStore(rootReducer, preloadedState, applyMiddleware(thunk))
-// }
-//
-// export default configureStore
-
 import { createStore } from 'redux'
 import rootReducer from '../reducers'
 
